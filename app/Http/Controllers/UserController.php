456cc31061faabc29f47778f56f3ee98d5d6974f
Takano Computer <goyataiko@gmail.com>
2023-08-16 00:36:38 +0900

    controller 関数追加

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,16 +1,25 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+
+use App\Models\User;
 
 class UserController extends Controller
 {
+    const JOB_SEEKR = 'seeker';
     public function createSeeker(){
         return view('user.seekr-register');
     }
 
     public function storeSeeker(){
-        return "hello";
+        User::create([
+            'name'=> request('name'),
+            'email'=> request('email'),
+            'password'=> bcrypt(request('password')),
+            'user_type'=> self::JOB_SEEKR
+        ]);
+
+        return back();
     }
 }
