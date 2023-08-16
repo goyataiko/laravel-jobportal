dbd12a8de4d2c4594d64d7fa8be78583930a9b1c
Takano Computer <goyataiko@gmail.com>
2023-08-16 10:27:07 +0900

    validation

--- resources/views/user/seeker-register.blade.php
+++ resources/views/user/seeker-register.blade.php
@@ -3,7 +3,7 @@
 
         <div class="container mt-5">
             <div class="row">
-                <div class="col-md-6 text-center">
+                <div class="col-md-6">
                     <h1>Looking for a job?</h1>
                     <h3>Please create an Account</h3>
                     <img src='{{asset("image/click-here.png")}}' alt="click-here">
@@ -16,15 +16,24 @@
                                 <div class="form-group">
                                     <label for="">Full Name</label>
                                     <input type="text" name="name" class="form-control">
+                                    @if($errors->has('name'))
+                                        <span class="text-danger">{{$errors->first('name')}}</span>
+                                    @endif
                                 </div>
                                 <div class="form-group">
                                     <label for="">Email</label>
                                     <input type="text" name="email" class="form-control">
                                 </div>
+                                @if($errors->has('name'))
+                                        <span class="text-danger">{{$errors->first('email')}}</span>
+                                @endif                                
                                 <div class="form-group">
                                     <label for="">Password</label>
                                     <input type="password" name="password" class="form-control">
                                 </div>
+                                @if($errors->has('name'))
+                                        <span class="text-danger">{{$errors->first('password')}}</span>
+                                @endif
                                 <br>
                                 <div class="form-group">
                                     <button type="submit" class="btn btn-primary">Register</button>
