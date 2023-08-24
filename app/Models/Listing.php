5de70d1cb1a282eb6d73db793ee9c9891743f028
Takano Computer <goyataiko@gmail.com>
2023-08-24 21:52:01 +0900

    post job ページ、controller

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Listing extends Model
 {
     use HasFactory;
+
+
+    public static $rules = array(
+        'feature_image' => 'required|mimes:png,jpeg,jpg|max:2048',
+        'title' => 'required|min:5',
+        'description' => 'required|min:20',
+        'roles' => 'required',
+        'job_type' => 'required',
+        'address' => 'required',
+        'date' => 'required',
+
+        'salary' => 'required',
+        // 'application_close_date',
+        'slug',
+    );
 }
--- resources/views/job/create.blade.php
+++ resources/views/job/create.blade.php
@@ -5,18 +5,35 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <h1>Post Job</h1>
-            <form action="" method="post">@csrf
+            <form action="{{route('job.store')}}" method="post" enctype="multipart/form-data">@csrf
+                <div class="form-group">
+                    <label for="feature_image">Feature Image</label>
+                    <input type="file" name="feature_image" id="feature_image" class="form-control">
+                    @if($errors->has('feature_image'))
+                    <span class="text-danger">{{$errors->first('feature_image')}}</span>
+                    @endif
+                </div>
+
                 <div class="form-group">
                     <label for="title">title</label>
                     <input type="text" name="title" id="title" class="form-control">
+                    @if($errors->has('title'))
+                    <span class="text-danger">{{$errors->first('title')}}</span>
+                    @endif
                 </div>
                 <div class="form-group">
                     <label for="description">Description</label>
-                    <textarea name="description" id="description" class="form-control"></textarea>
+                    <textarea name="description" id="description" class="form-control summernote"></textarea>
+                    @if($errors->has('description'))
+                    <span class="text-danger">{{$errors->first('description')}}</span>
+                    @endif
                 </div>
                 <div class="form-group">
                     <label for="roles">Roles and Responsibility</label>
-                    <textarea name="roles" id="roles" class="form-control"></textarea>
+                    <textarea name="roles" id="roles" class="form-control summernote"></textarea>
+                    @if($errors->has('roles'))
+                    <span class="text-danger">{{$errors->first('roles')}}</span>
+                    @endif
                 </div>
 
                 <div class="form-group">
@@ -37,15 +54,31 @@
                         <input type="radio" name="job_type" id="contract" value="contract" class="form-check-input">
                         <label for="contract" class="form-check-label">Contract</label>
                     </div>
+                    @if($errors->has('job_type'))
+                    <span class="text-danger">{{$errors->first('job_type')}}</span>
+                    @endif
                 </div>
 
                 <div class="form-group">
                     <label for="address">Address</label>
                     <input type="text" name="address" id="address" class="form-control">
+                    @if($errors->has('address'))
+                    <span class="text-danger">{{$errors->first('address')}}</span>
+                    @endif
+                </div>
+                <div class="form-group">
+                    <label for="salary">Salary</label>
+                    <input type="text" name="salary" id="salary" class="form-control">
+                    @if($errors->has('salary'))
+                    <span class="text-danger">{{$errors->first('salary')}}</span>
+                    @endif
                 </div>
                 <div class="form-group">
                     <label for="date">Date</label>
-                    <input type="date" name="date" id="date" class="form-control">
+                    <input type="text" name="date" id="datepicker" class="form-control">
+                    @if($errors->has('date'))
+                    <span class="text-danger">{{$errors->first('date')}}</span>
+                    @endif
                 </div>
                 <div class="form-group mt-4">
                     <button type="submit" class="btn btn-success">Post a Job</button>
@@ -56,4 +89,9 @@
     </div>
 </div>
 
+<style>
+    .note-insert {
+        display: none;
+    }
+</style>
 @endsection
