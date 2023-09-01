5ccf0e766c75d3e5964e80bda7faf588c7af7ca5
Takano Computer <goyataiko@gmail.com>
2023-09-01 15:43:09 +0900

    pond利用resume 提出

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,45 +1,57 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-  </head>
-  <body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 
-    <nav class="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
-        <div class="container">
-            <a class="navbar-brand" href="/">LaraJobPortal</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav ms-auto">
-                <a class="nav-link active" aria-current="page" href="/">Home</a>
-                @if(!@Auth::check())
-                <a class="nav-link" href="{{route('login')}}">Login</a>
-                  <a class="nav-link" href="{{route('create.seeker')}}">Job Seeker</a>
-                  <a class="nav-link" href="{{route('create.employer')}}">Employer</a>
-                @endif
-                @if(@Auth::check())
-                  <a class="nav-link active" aria-current="page" href="{{route('seeker.profile')}}">profile</a>
-                  <a class="nav-link" id="logout" href="#">LogOut</a>
-                @endif
-                <form id="form_logout" action="{{route('logout')}}" method="post">@csrf</form>
-            </div>
-            </div>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Bootstrap demo</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  <!-- upload기능 -->
+  <link href="https://unpkg.com/filepond@^4/dist/filepond.css" rel="stylesheet" />
+</head>
+
+<body>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  <script src="https://unpkg.com/filepond@^4/dist/filepond.js"></script>
+
+  <nav class="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
+    <div class="container">
+      <a class="navbar-brand" href="/">LaraJobPortal</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav ms-auto">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+          @if(!@Auth::check())
+          <a class="nav-link" href="{{route('login')}}">Login</a>
+          <a class="nav-link" href="{{route('create.seeker')}}">Job Seeker</a>
+          <a class="nav-link" href="{{route('create.employer')}}">Employer</a>
+          @endif
+          @if(@Auth::check())
+          <a class="nav-link active" aria-current="page" href="{{route('seeker.profile')}}">profile</a>
+          <a class="nav-link" id="logout" href="#">LogOut</a>
+          @endif
+          <form id="form_logout" action="{{route('logout')}}" method="post">@csrf</form>
         </div>
-    </nav>
-    
-    @yield('content')
-    <script>
-      let logout = document.getElementById('logout');
-      let form = document.getElementById('form_logout');
-      logout.addEventListener('click', function(){
-        form.submit();
-      })
-    </script>
-  </body>
+      </div>
+    </div>
+  </nav>
+
+  @yield('content')
+  <script>
+    let logout = document.getElementById('logout');
+    let form = document.getElementById('form_logout');
+    logout.addEventListener('click', function() {
+      form.submit();
+    })
+
+    $(document).on('click', '#modal', function(e) {
+      // 이벤트를 원래의 modal에 전달합니다.
+      e.stopPropagation();
+      $(this).modal('show');
+    });
+  </script>
+</body>
+
 </html>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -6,11 +6,9 @@
         <div class="col-md-8">
             <div class="card">
                 @if($listing->feature_image)
-                <img src="{{Storage::url($listing->feature_image)}}" class="card-img-top" alt="Cover Image"
-                height="150" style="object-fit: cover;">
+                <img src="{{Storage::url($listing->feature_image)}}" class="card-img-top" alt="Cover Image" height="150" style="object-fit: cover;">
                 @else
-                <img src="https://placehold.co/800" class="card-img-top" alt="Cover Image" 
-                height="150" style="object-fit: cover;">
+                <img src="{{asset('image/jobHeader.jpg')}}" class="card-img-top" alt="Cover Image" height="150" style="object-fit: cover;">
                 @endif
                 <div class="card-body">
                     <h2 class="card-title">{{$listing->title}}</h2>
@@ -18,7 +16,6 @@
                     <p>Salary: ￥{{number_format($listing->salary)}}</p>
                     <p>Address: {{$listing->address}}</p>
                     <h4 class="mt-4">Description</h4>
-                    <!-- html태그를 불러올때는 아래와 같이 {!! 내용물 !!} -->
                     <p class="card-text">{!!$listing->description!!}</p>
 
                     <h4>Roles and Responsibilities</h4>
@@ -26,11 +23,71 @@
 
                     <p class="card-text mt-4">Application closing date: {{$listing->application_close_date}}</p>
 
+                    @if($listing->profile->resume)
                     <a href="#" class="btn btn-primary mt-3">Apply Now</a>
+                    @else
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#applyModal">
+                        Apply
+                    </button>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="applyModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Upload Resume</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <input type="file" />
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" id="btnApply" class="btn btn-primary" disabled>Apply</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    @endif
                 </div>
             </div>
         </div>
     </div>
 </div>
 
+<script>
+    // Get a reference to the file input element
+    const inputElement = document.querySelector('input[type="file"]');
+
+    // Create a FilePond instance
+    const pond = FilePond.create(inputElement);
+
+    pond.setOptions({
+        server: {
+            url: '/',
+            process: {
+                method: 'POST',
+                withCredentials: false,
+                headers: { //@csrf 대신 이것을 붙여줌 
+                    'X-CSRF-TOKEN': '{{csrf_token()}}'
+                },
+                //데이터를 받음
+                ondata: (formData) => {
+                    console.log(pond.getFiles()[0].file);
+                    formData.append('file', pond.getFiles()[0].file, pond.getFiles()[0].file.name);
+
+                    return formData;
+                },
+                //성공하면 로드함
+                onload: (response) => {
+                    document.getElementById('btnApply').removeAttribute('disabled')
+                },
+                //실패하면 여기 에러
+                onerror: (response) => {
+                    console.log("error while uploading.", response);
+                },
+            },
+        },
+    });
+</script>
 @endsection
