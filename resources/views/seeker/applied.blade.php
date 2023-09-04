c72f853a820780f61bd3413ed09f3dd44b2f8d9a
Takano Computer <goyataiko@gmail.com>
2023-09-04 14:37:46 +0900

    recent added

--- resources/views/seeker/applied.blade.php
+++ resources/views/seeker/applied.blade.php
@@ -2,17 +2,21 @@
 @section('content')
 
 <div class="container mt-5">
-    <div class="row justify-content-center">
-        <div class="row mt-5">
-            <div class="col-md-8">
+    <div class="row">
+        <div class="row mt-5 justify-content-center">
+            <div class="col-md-6">
                 <h3>Applied Jobs</h3>
                 @foreach($users as $user)
                 @foreach($user->listings as $listing)
-                <div class="card mt-3">
-                    <div class="card-body">
-                        <h5 class="card-title">{{$listing->title}}</h5>
-                        <p class="card-text">Applied: {{$listing->pivot->created_at}}</p>
-                        <a href="{{route('job.show', [$listing->slug])}}" class="btn btn-primary">View</a>
+                <div class="card mb-1 p-1">
+                    <div class="card-body d-flex justify-content-between">
+                        <div>
+                            <h5 class="card-title">{{$listing->title}}</h5>
+                            <p class="card-text">Applied: {{$listing->pivot->created_at}}</p>
+                        </div>
+                        <div>
+                            <a href="{{route('job.show', [$listing->slug])}}" class="btn btn-primary">View</a>
+                        </div>
                     </div>
                 </div>
                 @endforeach
